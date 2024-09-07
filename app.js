f131bb662a3c71471a57e12b759c787ca0754d9a
Enzo Silva <esilvavalencuela@gmail.com>
2024-09-06 23:37:41 -0400

    Versao Inicial

---
+++ app.js
@@ -0,0 +1,59 @@
+function pesquisar() {
+  // Obtém a seção onde os resultados da pesquisa serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Digite o nome do atleta</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  // Inicializa uma string vazia para armazenar os resultados formatados
+  let resultados = "";
+  let nome = "";
+  let descricao = "";
+  let tags = "";
+  let titulos = "";
+
+  // Itera sobre cada dado na lista de dados
+  for (let dado of dados) {
+    nome = dado.nome.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    titulos = dado.titulos.toLowerCase();
+
+    // se titulo includes campoPesquisa
+    if (
+      nome.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa) ||
+      titulos.includes(campoPesquisa)
+    ) {
+      // Cria um novo elemento
+      resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href="${dado.pagina}" target="_blank"
+            >${dado.nome}</a
+          >
+        </h2>
+        <p class="descricao-meta">${dado.descricao}
+        </p>
+        <a href="${dado.link}" target="_blank"
+          >Mais informações</a
+        >
+      </div>
+    `;
+    }
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>";
+  }
+
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,293 @@
+let dados = [
+  //Lionel Messi
+  {
+    nome: "Lionel Messi",
+    descricao:
+      "Argentino, considerado um dos melhores da história, com dribles rápidos, controle de bola impecável e grande visão de jogo. Brilhou no Barcelona e agora joga no Inter Miami",
+    pagina: "https://www.instagram.com/leomessi/",
+    link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+    tags: "argentina, melhor do mundo, melhores da historia, ET, Alienigena",
+    titulos: "copa do mundo, champions league, UEFA",
+  },
+  //Cristiano Ronaldo
+  {
+    nome: "Cristiano Ronaldo",
+    descricao:
+      "Português, famoso por sua força, velocidade e habilidade aérea. Um dos maiores goleadores, jogou no Manchester United, Real Madrid, Juventus e atualmente no Al-Nassr.",
+    pagina: "https://www.instagram.com/cristiano/",
+    link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+    tags: "Portugal, melhor do mundo, melhores da historia, CR7, Papai Cris, Máquina, Al Nassr, SIUUUUU",
+    titulos: "champions league, UEFA",
+  },
+  //Neymar Júnior
+  {
+    nome: "Neymar Jr.",
+    descricao:
+      "Brasileiro criativo e habilidoso, conhecido por seus dribles e estilo carismático. Atacante versátil, passou pelo Santos, Barcelona e PSG, e é uma estrela global do futebol.",
+    pagina: "https://www.instagram.com/neymarjr/",
+    link: "https://pt.wikipedia.org/wiki/Neymar",
+    tags: "Brasil, menino ney, melhor driblador, Al Hillal, cai cai",
+    titulos: "champions league, UEFA",
+  },
+  //Pelé
+  {
+    nome: "Pelé",
+    descricao:
+      "Considerado por muitos o maior jogador de todos os tempos, o brasileiro Pelé encantou o mundo com sua habilidade e gols. Foi tricampeão mundial com a Seleção Brasileira.",
+    pagina: "https://www.instagram.com/pele/",
+    link: "https://pt.wikipedia.org/wiki/Pelé",
+    tags: "Brasil, rei do futebol, melhores da historia, Santos, tricampeão, pele",
+    titulos: "copa do mundo",
+  },
+  //Ibrahimović
+  {
+    nome: "Zlatan Ibrahimović",
+    descricao:
+      "O sueco Zlatan é conhecido por sua personalidade forte, técnica apurada e gols espetaculares. Atuou em diversos clubes europeus, como Milan, PSG e Manchester United.",
+    pagina: "https://www.instagram.com/iamzlatanibrahimovic/",
+    link: "https://pt.wikipedia.org/wiki/Zlatan_Ibrahimović",
+    tags: "Suécia, Ibra, ego, PSG, Milan,",
+    titulos: "Ligue 1, Serie A",
+  },
+  //Kylian Mbappé
+  {
+    nome: "Kylian Mbappé",
+    descricao:
+      "O francês Mbappé é uma das maiores promessas do futebol mundial. Rápido, habilidoso e com grande potencial, ele já conquistou diversos títulos pelo PSG e pela Seleção Francesa.",
+    pagina: "https://www.instagram.com/k.mbappe/",
+    link: "https://pt.wikipedia.org/wiki/Kylian_Mbappé",
+    tags: "França, PSG, jovem prodígio, mundial, mbappe, melhor do mundo",
+    titulos: "Ligue 1, Copa do Mundo",
+  },
+  //Ronaldinho Gaúcho
+  {
+    nome: "Ronaldinho Gaúcho",
+    descricao:
+      "O brasileiro Ronaldinho é conhecido por seu estilo de jogo alegre e habilidoso. Conquistou o mundo com seus dribles e gols, sendo um dos maiores ídolos do Barcelona.",
+    pagina: "https://www.instagram.com/ronaldinho/",
+    link: "https://pt.wikipedia.org/wiki/Ronaldinho",
+    tags: "Brasil, sorriso, mágico, Barcelona, ronaldinho gaucho",
+    titulos: "Champions League, Copa do Mundo",
+  },
+  //Erling Haaland
+  {
+    nome: "Erling Haaland",
+    descricao:
+      "O norueguês Erling Haaland é conhecido por sua força física e faro de gol. Atualmente no Manchester City, ele vem quebrando recordes e se tornando um dos maiores atacantes do mundo.",
+    pagina: "https://www.instagram.com/erlinghaaland/",
+    link: "https://pt.wikipedia.org/wiki/Erling_Haaland",
+    tags: "Noruega, Manchester City, goleador, jovem talento, haaland, melhor do mundo",
+    titulos: "Bundesliga, Premier League",
+  },
+  //Vinícius Júnior
+  {
+    nome: "Vinícius Júnior",
+    descricao:
+      "O brasileiro Vinícius Júnior é conhecido por sua velocidade e dribles. Atualmente no Real Madrid, ele se tornou um dos principais jogadores do time e da seleção brasileira.",
+    pagina: "https://www.instagram.com/vinijr/",
+    link: "https://pt.wikipedia.org/wiki/Vinícius_Júnior",
+    link: "https://pt.wikipedia.org/wiki/Vinícius_Júnior",
+    tags: "Brasil, Real Madrid, habilidade, drible, vini jr, malvadeza, melhor do mundo",
+    titulos: "La Liga, Champions League",
+  },
+  //Kevin De Bruyne
+  {
+    nome: "Kevin De Bruyne",
+    descricao:
+      "O belga Kevin De Bruyne é considerado um dos melhores meio-campistas do mundo. Atualmente no Manchester City, ele se destaca pela visão de jogo e passes precisos.",
+    pagina: "https://www.instagram.com/kevindebruyne/",
+    link: "https://pt.wikipedia.org/wiki/Kevin_De_Bruyne",
+    tags: "Bélgica, Manchester City, passe, visão de jogo",
+    titulos: "Premier League",
+  },
+  //Marcelo
+  {
+    nome: "Marcelo",
+    descricao:
+      "O brasileiro Marcelo é um dos maiores laterais-esquerdos da história, conhecido por sua habilidade, velocidade e cruzamentos precisos. Passou a maior parte da carreira no Real Madrid.",
+    pagina: "https://www.instagram.com/Marcelo/",
+    link: "https://pt.wikipedia.org/wiki/Marcelo_Vieira",
+    tags: "Brasil, Real Madrid, lateral-esquerdo, habilidade",
+    titulos: "Champions League, La Liga",
+  },
+  //Mesut Özil
+  {
+    nome: "Mesut Özil",
+    descricao:
+      "O alemão Mesut Özil é um meia-atacante com grande visão de jogo e passes precisos. Teve passagens marcantes por Real Madrid e Arsenal.",
+    pagina: "https://www.instagram.com/m10_official/",
+    link: "https://pt.wikipedia.org/wiki/Mesut_Özil",
+    tags: "Alemanha, Real Madrid, Arsenal, meia-atacante",
+    titulos: "Premier League, Copa do Mundo",
+  },
+  //Manuel Neuer
+  {
+    nome: "Manuel Neuer",
+    descricao:
+      "O alemão Manuel Neuer revolucionou a posição de goleiro com seu estilo de jogo com os pés. É considerado um dos melhores goleiros da história e atua no Bayern de Munique.",
+    pagina: "https://www.instagram.com/manuel_neuer/",
+    link: "https://pt.wikipedia.org/wiki/Manuel_Neuer",
+    tags: "Alemanha, Bayern de Munique, goleiro",
+    titulos: "Bundesliga, Champions League, Copa do Mundo",
+  },
+  //Gianluigi Buffon
+  {
+    nome: "Gianluigi Buffon",
+    descricao:
+      "O italiano Gianluigi Buffon é uma lenda do futebol, conhecido por sua longevidade, reflexos e liderança. Defendeu as cores de Juventus e Parma.",
+    pagina: "https://www.instagram.com/gianluigibuffon/",
+    link: "https://pt.wikipedia.org/wiki/Gianluigi_Buffon",
+    tags: "Itália, Juventus, Parma, goleiro",
+    titulos: "Serie A, Copa do Mundo",
+  },
+  //Diego Maradona
+  {
+    nome: "Diego Maradona",
+    descricao:
+      "O argentino Diego Maradona é considerado um dos maiores jogadores de todos os tempos, famoso por sua habilidade, dribles e gols memoráveis. Liderou a Argentina à conquista da Copa do Mundo de 1986.",
+    pagina: "https://www.instagram.com/maradona/", // Verificar se a conta existe
+    link: "https://pt.wikipedia.org/wiki/Diego_Maradona",
+    tags: "Argentina, Napoli, melhores da historia, Boca Juniors, drible, gol",
+    titulos: "Copa do Mundo, Serie A",
+  },
+  //Garrincha
+  {
+    nome: "Garrincha",
+    descricao:
+      "O brasileiro Garrincha era conhecido por sua alegria, dribles desconcertantes e habilidade com a bola. Foi um dos principais jogadores da seleção brasileira campeã da Copa do Mundo de 1962.",
+    pagina: "", // Não há perfil oficial do Garrincha em redes sociais
+    link: "https://pt.wikipedia.org/wiki/Garrincha",
+    tags: "Brasil, Botafogo, Copa do Mundo, drible",
+    titulos: "Copa do Mundo",
+  },
+  //Eusébio
+  {
+    nome: "Eusébio",
+    descricao:
+      "O português Eusébio, conhecido como a Pantera Negra, foi um dos maiores atacantes da história. Se destacou no Benfica e na seleção portuguesa.",
+    pagina: "", // Não há perfil oficial do Eusébio em redes sociais
+    link: "https://pt.wikipedia.org/wiki/Eusébio",
+    tags: "Portugal, Benfica, atacante",
+    titulos: "Taça dos Campeões Europeus",
+  },
+  //Zico
+  {
+    nome: "Zico",
+    descricao:
+      "O brasileiro Zico, conhecido como o Galinho de Quintal, era um meia-atacante com grande técnica e visão de jogo. Liderou o Flamengo e a seleção brasileira em diversas conquistas.",
+    pagina: "https://www.instagram.com/zicooficial/",
+    link: "https://pt.wikipedia.org/wiki/Zico",
+    tags: "Brasil, Flamengo, meia-atacante",
+    titulos: "Campeonato Brasileiro",
+  },
+  //Zinedine Zidane
+  {
+    nome: "Zinedine Zidane",
+    descricao:
+      "O francês Zinedine Zidane era um meia elegante e habilidoso. Conquistou a Copa do Mundo de 1998 pela França e é considerado um dos maiores jogadores da história.",
+    pagina: "https://www.instagram.com/zidane/",
+    link: "https://pt.wikipedia.org/wiki/Zinedine_Zidane",
+    tags: "França, Real Madrid, meia, zidane",
+    titulos: "Copa do Mundo, Champions League",
+  },
+  //Ronaldo Fenômeno
+  {
+    nome: "Ronaldo Fenômeno",
+    descricao:
+      "O brasileiro Ronaldo Luís Nazário de Lima, conhecido como Ronaldo Fenômeno, foi um dos atacantes mais rápidos e habilidosos da história. Conquistou duas Copas do Mundo com a seleção brasileira.",
+    pagina: "https://www.instagram.com/ronaldo/",
+    link: "https://pt.wikipedia.org/wiki/Ronaldo_Luís_Nazário_de_Lima",
+    tags: "Brasil, Inter de Milão, Real Madrid, atacante, fenomeno, ronaldo",
+    titulos: "Copa do Mundo, Serie A, La Liga",
+  },
+  //Rivaldo
+  {
+    nome: "Rivaldo",
+    descricao:
+      "O brasileiro Rivaldo foi um dos maiores jogadores da sua geração, conhecido por sua versatilidade, chutes potentes e dribles. Foi fundamental para a conquista da Copa do Mundo de 2002.",
+    pagina: "https://www.instagram.com/rivaldo/", // Verificar se a conta existe
+    link: "https://pt.wikipedia.org/wiki/Rivaldo",
+    tags: "Brasil, Corinthians, Barcelona, meia-atacante",
+    titulos: "Copa do Mundo, La Liga",
+  },
+  //Kaká
+  {
+    nome: "Kaká",
+    descricao:
+      "O brasileiro Kaká foi um meia elegante e habilidoso, conhecido por sua visão de jogo e passes precisos. Conquistou a Bola de Ouro em 2007 e foi fundamental para a conquista da Champions League com o Milan.",
+    pagina: "https://www.instagram.com/kaka/",
+    link: "https://pt.wikipedia.org/wiki/Kaká",
+    tags: "Brasil, Milan, Real Madrid, meia, melhor do brasil",
+    titulos: "Bola de Ouro, Champions League",
+  },
+  //Robert Lewandowski
+  {
+    nome: "Robert Lewandowski",
+    descricao:
+      "O polonês Robert Lewandowski é um dos maiores artilheiros da atualidade, conhecido por sua força física, finalização precisa e oportunismo. Atualmente joga no Barcelona.",
+    pagina: "https://www.instagram.com/robert_lewandowski/",
+    link: "https://pt.wikipedia.org/wiki/Robert_Lewandowski",
+    tags: "Polônia, Bayern de Munique, Barcelona, atacante",
+    titulos: "Bundesliga",
+  },
+  //Virgil Van Dijk
+  {
+    nome: "Virgil van Dijk",
+    descricao:
+      "Zagueiro holandês, conhecido por sua altura, força física, e habilidade no jogo aéreo. Considerado um dos melhores zagueiros do mundo, foi fundamental para a conquista da Champions League com o Liverpool.",
+    pagina: "https://www.instagram.com/virgilvandijk/",
+    link: "https://pt.wikipedia.org/wiki/Virgil_van_Dijk",
+    tags: "Holanda, zagueiro, Liverpool, gigante, Champions League, van dijk",
+    titulos: "champions league, UEFA",
+  },
+  //Gavi
+  {
+    nome: "Gavi",
+    descricao:
+      "Jovem meio-campista espanhol, conhecido por sua técnica apurada, visão de jogo e personalidade. Revelado pelo Barcelona, é considerado uma das maiores promessas do futebol mundial.",
+    pagina: "https://www.instagram.com/gavimira/",
+    link: "https://pt.wikipedia.org/wiki/Gavi",
+    tags: "Espanha, meio-campista, Barcelona, La Masia, jovem promessa",
+    titulos: "La Liga",
+  },
+  //Christian Eriksen
+  {
+    nome: "Christian Eriksen",
+    descricao:
+      "Dinamarquês conhecido por sua visão de jogo e passes precisos. Meia com excelente controle de bola, jogou em clubes como Ajax, Tottenham e Inter de Milão, e atualmente no Manchester United.",
+    pagina: "https://www.instagram.com/christianeriksenofficial/", // Substitua pela página oficial do Instagram
+    link: "https://pt.wikipedia.org/wiki/Christian_Eriksen",
+    tags: "Dinamarca, passe, visão de jogo, Manchester United",
+    titulos: "Serie A",
+  },
+  //Jamal Musiala
+  {
+    nome: "Jamal Musiala",
+    descricao:
+      "Jovem talento alemão, meia-atacante com grande potencial. Destaca-se pela velocidade, dribles e finalização. Atualmente joga no Bayern de Munique.",
+    pagina: "https://www.instagram.com/jamalmusiala/", // Substitua pela página oficial do Instagram
+    link: "https://pt.wikipedia.org/wiki/Jamal_Musiala",
+    tags: "Alemanha, jovem talento, Bayern de Munique",
+    titulos: "Bundesliga",
+  },
+  //Endrick
+  {
+    nome: "Endrick",
+    descricao:
+      "Promissor atacante brasileiro, considerado uma das maiores promessas do futebol mundial. Conhecido por sua habilidade, velocidade e faro de gol. Atualmente no Palmeiras.",
+    pagina: "https://www.instagram.com/endrick/", // Substitua pela página oficial do Instagram
+    link: "https://pt.wikipedia.org/wiki/Endrick_(futebolista)",
+    tags: "Brasil, jovem talento, Palmeiras",
+    titulos: "Brasileirão",
+  },
+  //Gabriel Barbosa
+  {
+    nome: "Gabriel Barbosa (Gabigol)",
+    descricao:
+      "Atacante brasileiro conhecido por sua finalização e habilidade. Destacou-se no Santos e atualmente joga no Flamengo. Artilheiro em diversas competições.",
+    pagina: "https://www.instagram.com/gabigol/",
+    link: "https://pt.wikipedia.org/wiki/Gabriel_Barbosa_(futebolista)",
+    tags: "Brasil, Gabigol, Flamengo, artilheiro",
+    titulos: "Brasileirão, Libertadores",
+  },
+];
